c9a290013e53ba924bd41f6e1d0e14520ccf564a
mohmmad-okasha <mhmmadokasha@gmail.com>
2023-06-01 14:20:06 +0300

    backend link dinamic

--- frontend/src/axios-api.js
+++ frontend/src/axios-api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+var currentURL = window.location.protocol + "//" + window.location.hostname + ":8000" + window.location.pathname;
 
-const domain_url = 'http://0.0.0.0:8000';
+const domain_url = currentURL;
 
 const my_api = axios.create({
     baseURL: domain_url,
